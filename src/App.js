6d7d4dad5679e7ae05b415ecfac716d9318cd744
Jason Van Malder <jasonvanmalder@gmail.com>
2018-07-01 14:38:09 +0200

    Changements mineurs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
+          <h2>Début du TFE</h2>
         </div>
         <p className="App-intro">
-          Hello Electron!
+          Hello World !
         </p>
       </div>
     );
