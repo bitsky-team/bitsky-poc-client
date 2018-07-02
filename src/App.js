c81e33592f68a3859af596f24ce6678f0b676f77
Sylv11 <sylvain.urbain.isf@gmail.com>
2018-07-02 17:03:38 +0200

    Login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
+import logo from './assets/img/logo.png';
+import $ from 'jquery';
 
 class App extends Component {
+
+  componentDidMount() {
+    setInterval(() => {
+      $('.single-form-subcontainer.right .container').width($('.single-form-subcontainer.right').width());
+      let img = $('.single-form-subcontainer.right .container img');
+      let container = $('.single-form-subcontainer.right');
+
+      img.css('top',(container.height()-img.height())/2 + 'px');
+      img.css('left',(container.width()-img.width())/2 + 'px');
+
+    }, 300);
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Début du TFE</h2>
+        <div className="single-form-container">
+          <div className="single-form-subcontainer left">
+           <img src={logo} alt="logo"/>
+           <div className="slogan">
+            <h2><strong>Vos</strong> amis,</h2>
+            <h2><strong>Vos</strong> souvenirs,</h2>
+            <h2><strong>Vos</strong> données,</h2>
+            <h2><strong>Chez vous.</strong></h2>
+           </div>
+          </div>
+          <div className="single-form-subcontainer right">
+            <div className="overlay"></div>
+            <div className="container">
+              <nav>
+                <ul>
+                  <li><a href="#">À propos</a></li>
+                  <li><a href="#">Support</a></li>
+                  <li><a href="#">Mises à jour</a></li>
+                  <li><a href="#">Documentation</a></li>
+                </ul>
+              </nav>
+              <img src={logo} alt="logo"/>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          Hello World !
-        </p>
       </div>
     );
   }
