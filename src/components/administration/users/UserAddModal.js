ea5117a8066c5b7fe1135b358a2184bc7c82093a
jasonvanmalder <jasonvanmalder@gmail.com>
2018-11-16 10:30:43 +0100

    UserAddModal - Front-end design

--- src/components/administration/users/UserAddModal.js
+++ src/components/administration/users/UserAddModal.js
@@ -36,26 +36,90 @@ export default class UserAddModal extends Component {
                         </Col>
                         <Col md={6}>
                             <FormGroup>
+                                <Label for="userAddRank">Rang</Label>
+                                <Input type="select" name="select" id="userAddRank">
+                                    <option>Utilisateur</option>
+                                    <option>Administrateur</option>
+                                </Input>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <Row form>
+                        <Col md={6}>
+                            <FormGroup>
                                 <Label for="userAddPassword">Mot de passe</Label>
                                 <Input type="password" name="userAddPassword" id="userAddPassword" placeholder="Mot de passe" />
                             </FormGroup>
                         </Col>
-                    </Row>
-                    <FormGroup row>
-                        <Label for="userAddRank" sm={2}>Rang</Label>
-                        <Col sm={3}>
-                            <Input type="select" name="select" id="userAddRank">
-                                <option>Utilisateur</option>
-                                <option>Administrateur</option>
-                            </Input>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="userAddPassword">Répétez le mot de passe</Label>
+                                <Input type="password" name="userAddRepeatPassword" id="userAddRepeatPassword" placeholder="Répétez le mot de passe" />
+                            </FormGroup>
                         </Col>
-                    </FormGroup>
+                    </Row>
                     <FormGroup row>
                         <Label for="userAddBiography" sm={2}>Biographie</Label>
                         <Col sm={10}>
                             <Input type="textarea" name="userAddBiography" id="userAddBiography" />
                         </Col>
                     </FormGroup>
+                    <Row form>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="userAddSex">Sexe</Label>
+                                <Input type="select" name="userAddSex" id="userAddSex">
+                                    <option>Homme</option>
+                                    <option>Femme</option>
+                                    <option>Autre</option>
+                                </Input>
+                            </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="userAddJob">Emploi</Label>
+                                <Input type="text" name="userAddJob" id="userAddJob" placeholder="Emploi" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <Row form>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="userAddBirthdate">Date de naissance</Label>
+                                <Input type="date" name="userAddBirthdate" id="userAddBirthdate" />
+                            </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="userAddBirthplace">Ville d'origine</Label>
+                                <Input type="text" name="userAddBirthplace" id="userAddBirthplace" placeholder="Ville d'origine" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <Row form>
+                        <Col md={6}>
+                            <FormGroup>
+                                 <Label for="userAddRelationshipstatus">Situation amoureuse</Label>
+                                <Input 
+                                    type="select" 
+                                    name="userAddRelationshipstatus" 
+                                    id="userAddRelationshipstatus"
+                                >
+                                    <option>Célibataire</option>
+                                    <option>En couple</option>
+                                    <option>Marié(e)</option>
+                                    <option>Veuf(ve)</option>
+                                    <option>Non précisé</option>
+                                </Input>
+                            </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="userAddLivingplace">Ville actuelle</Label>
+                                <Input type="text" name="userAddLivingplace" id="userAddLivingplace" placeholder="Ville actuelle" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
                 </Form>
             </ModalBody>
             <ModalFooter>
