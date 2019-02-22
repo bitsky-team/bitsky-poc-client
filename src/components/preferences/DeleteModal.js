1bb5b8fb7e5c4eb32d159bae561ba4dab03869d3
Sylv11 <sylvain.urbain.isf@gmail.com>
2019-02-22 10:54:06 +0100

    User preferences delete user

---
+++ src/components/preferences/DeleteModal.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react'
+import {Modal, ModalHeader, ModalBody, ModalFooter, Button, Alert} from 'reactstrap'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCheck, faTimes} from '@fortawesome/free-solid-svg-icons'
+import {config} from '../../config'
+import axios from 'axios'
+import qs from 'qs'
+import jwtDecode from "jwt-decode";
+import {toast} from "react-toastify";
+
+const DeleteModal = (props) => {
+
+    const [error, setError] = useState(false)
+    const [session] = useState(jwtDecode(localStorage.getItem('token')))
+
+    const deleteAccount = async () => {
+        const userId = session.id
+        const response = await axios.post(`${config.API_ROOT}/delete_user`, qs.stringify({
+            token: localStorage.getItem('token'),
+            uniq_id: localStorage.getItem('id'),
+            user_id: userId
+        }))
+        const {success} = response.data
+
+        if (success) {
+            window.location.href = '/login'
+        } else {
+            setError(true)
+            toast.error('Votre compte n\'a pas pu être supprimé !', {
+                autoClose: 5000,
+                position: toast.POSITION.BOTTOM_RIGHT,
+            })
+        }
+    }
+
+    return (
+        <Modal isOpen={props.open} toggle={props.toggleDeleteModal} className="user-modal">
+            <ModalHeader style={{background: 'white'}}>
+                Suppression de mon compte
+            </ModalHeader>
+            <ModalBody style={{background: 'white'}}>
+                {!error ?
+                    <div>
+                        Êtes-vous sûr de vouloir supprimer votre compte ?
+                        <br/><br/>
+                        En supprimant votre compte, vous perdez l'accès à celui-ci et cela entraine la suppression de toutes les données vous concernant (publications, commentaires, fichiers, messages, etc).
+                        <br/>
+                        Si ces données n'ont pas été sauvegardées ou enregistrées à un autre emplacement, elles seront perdues.
+                    </div>
+                    : <Alert color='danger'>Impossible d'effectuer cette opération</Alert>
+                }
+            </ModalBody>
+            <ModalFooter style={{background: 'white'}}>
+                <Button className="modal-choice" color="primary" onClick={deleteAccount}><FontAwesomeIcon
+                    icon={faCheck}/></Button>{' '}
+                <Button className="modal-choice" color="secondary"
+                        onClick={props.toggleDeleteModal}><FontAwesomeIcon icon={faTimes}/></Button>
+            </ModalFooter>
+        </Modal>
+    )
+}
+
+export default DeleteModal
--- src/components/preferences/UserPreferencesAccountPage.js
+++ src/components/preferences/UserPreferencesAccountPage.js
@@ -1,12 +1,48 @@
-import React, {Fragment} from 'react'
-import {Container, Row, Col} from 'reactstrap'
-import Navbar from '../common/template/Navbar'
-import UserPreferencesSideMenu from './common/UserPreferencesSideMenu'
+import React, {Fragment, useState} from 'react'
+import {Container, Row, Col, Button} from 'reactstrap'
+import Navbar from '../../components/common/template/Navbar'
+import UserPreferencesSideMenu from '../preferences/common/UserPreferencesSideMenu'
+import styled from "styled-components";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTrashAlt} from "@fortawesome/free-solid-svg-icons";
+import DeleteModal from './DeleteModal'
+import axios from "axios";
+import {config} from "../../config";
+import qs from "qs";
+import jwtDecode from "jwt-decode";
+import {toast} from 'react-toastify'
 
 const UserPreferencesAccountPage = () => {
+
+  const [open, setOpen] = useState(false)
+
+  const UserAccountContainer = styled.div`
+    &&& {
+      text-align: left;
+    }
+  `
+
+  const DeleteButton = styled(Button)`
+    
+    &&& {
+        background-color: rgb(238, 117, 117);
+        border: 1px solid rgb(238, 117, 117);
+    }
+    
+    &&&:hover,  &&&&:active, &&&:focus{
+        background-color: rgb(234, 81, 81);
+        border: 1px solid rgb(234, 81, 81);
+    }
+  `
+
+  const toggleDeleteModal = () => {
+    setOpen(!open)
+  }
+
   return (
     <Fragment>
       <Navbar />
+      <DeleteModal open={open} toggleDeleteModal={toggleDeleteModal}/>
       <Container>
         <Row>
           <Col>
@@ -19,9 +55,18 @@ const UserPreferencesAccountPage = () => {
                   <Container>
                     <Row>
                       <Col>
-                        <div className="user-container margin-top-10">
-                          Account
-                        </div>
+                        <UserAccountContainer className="user-container margin-top-10">
+                          <h4 style={{marginBottom: '15px'}}>
+                            Supprimer définitivement mon compte et mes informations
+                          </h4>
+                          <DeleteButton
+                              size="lg"
+                              block
+                              onClick={toggleDeleteModal}
+                          >
+                            <FontAwesomeIcon icon={faTrashAlt} />
+                          </DeleteButton>
+                        </UserAccountContainer>
                       </Col>
                     </Row>
                   </Container>
