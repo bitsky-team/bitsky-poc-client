ee2cb47df16d1de155123738ddfd2ee9d5ecacbb
Jason Van Malder <jasonvanmalder@gmail.com>
2019-03-03 18:21:49 +0100

    Link creation

--- src/components/administration/links/AdministrationLinksModal.js
+++ src/components/administration/links/AdministrationLinksModal.js
@@ -19,6 +19,7 @@ import {faHandshake} from '@fortawesome/free-regular-svg-icons/faHandshake'
 import axios from 'axios'
 import qs from 'qs'
 import {toast} from 'react-toastify'
+import {config} from '../../../config'
 
 export const AdministrationLinksModal = props => {
   const [linkName, setLinkName] = useState('')
@@ -29,6 +30,30 @@ export const AdministrationLinksModal = props => {
 
   const {open, toggleLinkModal} = props
 
+  const createLink = async (name, key, active) => {
+    const response = await axios.post(
+      `${config.API_ROOT}/create_link`,
+      qs.stringify({
+        uniq_id: localStorage.getItem('id'),
+        token: localStorage.getItem('token'),
+        bitsky_name: name,
+        bitsky_key: key,
+        link_state: active
+      })
+    )
+  
+    const {success, link, message: error} = response.data
+  
+    if (success) {
+      console.log(link)
+    } else {
+      toast.error(`Impossible de créer la liaison ! (${error})`, {
+        autoClose: 5000,
+        position: toast.POSITION.BOTTOM_RIGHT,
+      })
+    }
+  }
+
   const etablishLink = async () => {
     setLinkNameError(false)
     setBitskyKeyError(false)
@@ -36,7 +61,7 @@ export const AdministrationLinksModal = props => {
     let isLinkCorrect = linkName && linkName.length >= 2
     let isKeyCorrect = bitskyKey && bitskyKey.length === 32
     let checkNotSameKey = bitskyKey !== props.senderKey
-    
+
     if (isLinkCorrect && isKeyCorrect && checkNotSameKey) {
       const response = await axios.post(
         `https://bitsky.be/link`,
@@ -45,17 +70,21 @@ export const AdministrationLinksModal = props => {
           receiverKey: bitskyKey,
         })
       )
-      
+
       const {success, data, message: error} = response.data
-      
-      if(success) {
-        if(data.first_agreement && data.second_agreement) {
+
+      if (success) {
+        if (data.first_agreement && data.second_agreement) {
+          createLink(linkName, bitskyKey, 1)
+          
           toast.success('La liaison est établie !', {
             autoClose: 5000,
             position: toast.POSITION.BOTTOM_RIGHT,
             className: 'notification-success',
           })
-        } else if(data.first_agreement && !data.second_agreement) {
+        } else if (data.first_agreement && !data.second_agreement) {
+          createLink(linkName, bitskyKey, 0)
+          
           toast.success('La liaison est créée et en attente de validation !', {
             autoClose: 5000,
             position: toast.POSITION.BOTTOM_RIGHT,
@@ -68,12 +97,12 @@ export const AdministrationLinksModal = props => {
           position: toast.POSITION.BOTTOM_RIGHT,
         })
       }
-  
+
       toggleLinkModal()
     } else {
       setLinkNameError(!isLinkCorrect)
       setBitskyKeyError(!isKeyCorrect)
-      if(!checkNotSameKey) {
+      if (!checkNotSameKey) {
         toast.error(`Veuillez entrer une clé valide !`, {
           autoClose: 5000,
           position: toast.POSITION.BOTTOM_RIGHT,
